5487e664cd58163cd2ec0327c590f9e1f502a176
SeanDanielOfficial <officialsuzuen@gmail.com>
2018-02-12 12:36:41 +0800

    Drawable files, layouts, java class, manifest

---
+++ app/src/main/java/officialsuzuen/google/com/trasearch/ChooseLayout.java
@@ -0,0 +1,35 @@
+package officialsuzuen.google.com.trasearch;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+
+/**
+ * Created by Del Mar on 2/12/2018.
+ */
+
+public class ChooseLayout extends AppCompatActivity {
+
+    Button btn, btn2;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_layout);
+
+        btn = (Button) findViewById(R.id.btnNonB);
+        btn2 = (Button) findViewById(R.id.btnYesB);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(ChooseLayout.this, Register.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
--- app/src/main/java/officialsuzuen/google/com/trasearch/Login.java
+++ app/src/main/java/officialsuzuen/google/com/trasearch/Login.java
@@ -1,38 +1,18 @@
 package officialsuzuen.google.com.trasearch;
 
-import android.content.Intent;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
-public class Login extends AppCompatActivity {
-    Button button;
-    FloatingActionButton floatingActionButton;
+/**
+ * Created by Del Mar on 2/12/2018.
+ */
+
+public class Login extends AppCompatActivity{
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        button = (Button) findViewById(R.id.loginBtn2);
-
-        floatingActionButton = (FloatingActionButton) findViewById(R.id.floatingButton2);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(Login.this, Register.class);
-                startActivity(i);
-            }
-        });
-
-        floatingActionButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(Login.this, Donate.class);
-                startActivity(i);
-            }
-        });
     }
 }
